9b3486a67db8be4e6c2625324dede2528abe1be8
Ghufran Fathur Rahman <82429851+franfathur@users.noreply.github.com>
2025-08-23 02:18:53 +0700

    Update index.js

--- src/Pages/Protocol/index.js
+++ src/Pages/Protocol/index.js
@@ -1,9 +1,5 @@
-import React, { useEffect } from 'react'
-import masker from '../../Img/masker.png'
-import cucitangan from '../../Img/cucitangan.png'
-import jagajarak from '../../Img/jagajarak.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import React, { useEffect } from 'react';
+
 // importing aos
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -11,10 +7,10 @@ import 'aos/dist/aos.css';
 export default function Protocol() {
     useEffect(() => {
         AOS.init();
-    }, [])
+    }, []);
 
     return (
         <>
         </>
-    )
+    );
 }
